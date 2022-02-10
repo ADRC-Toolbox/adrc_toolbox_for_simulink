c790fc881233a344aacc671bd52298b5be5a2f51
k-lak <krzysztof.lakomy92@gmail.com>
2022-02-10 17:57:09 +0100

    Correct library generation for 2018b

--- config/export_to_2018b.m
+++ config/export_to_2018b.m
@@ -3,12 +3,12 @@ function export_to_2018b(path)
     
     for i=1:numel(path)
         temp_path = filepath{i} + "/" + filename{i} + "_TEMP" + extension{i};
-        load_system(filename{i});
-        Simulink.exportToVersion(filename{i}, temp_path,'R2018b'); % creates a temp file (because direct override in forbidden)
-        close_system(filename{i});
+        movefile(path{i}, temp_path)
+        temp_filename = filename{i} + "_TEMP";
+        load_system(temp_filename);
+        Simulink.exportToVersion(temp_filename, path{i},'R2018b'); % creates a temp file (because direct override in forbidden)
+        close_system(temp_filename);
 
-        delete(path{i});
-        movefile(temp_path, path{i})
+        delete(temp_path);
     end
 end
-
